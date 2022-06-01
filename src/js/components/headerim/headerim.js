0950f26ae7efb3be170834b14aec8e78c095a588
Dmitriy <Dima_kozlov20011@mail.ru>
2022-06-02 01:25:17 +0300

    Test

--- src/js/components/headerim/headerim.js
+++ src/js/components/headerim/headerim.js
@@ -4,10 +4,9 @@ const headerim = document.querySelector('#headerim');
 
 let headerimHtml = generalHeaderim.map((a) => {
     return `
-    <div class="col-md-4">
         <div class = "bvn">
 
-        <img src="${a.img}" alt="" style=" height: 600px;">
+        <img src="${a.img}" width="1000">
         </div>
     `
 }).join(" ");
