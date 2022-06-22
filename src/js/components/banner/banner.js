2196996d8851a3d8534410a88cf23152ff09ded5
GogaKler <gogakler@gmail.com>
2022-06-23 00:43:48 +0300

    adaptive banner_1

--- src/js/components/banner/banner.js
+++ src/js/components/banner/banner.js
@@ -11,21 +11,19 @@ let bannerHtmlLeft = generalBannerLeft.map((a) => {
         <div class="banner__img">
             <img class="image" src="${a.img}" alt="photo" />
         </div>
-        <button class="banner__button" style="background-color: #FF7C00; color: black;
-         font-weight: 600; font-size: 20px; margin-top: 50px; padding: 5px 10px; border: transparent; border-radius: 5px">${a.text}</button>
+        <button class="button button__orange">${a.text}</button>
     </div>
     `
 }).join(" ");
 
 let bannerHtmlMid = generalBannerMid.map((a) => {
     return `
-    <div class="col-md-4 banner__card" style = "border-left: 1px solid #FF7C00; border-right: 1px solid #FF7C00;">
+    <div class="col-md-4 banner__card">
         <p class="banner__title">${a.title}</p>
         <div class="banner__img">
             <img class="image" src="${a.img}" alt="photo" />
         </div>
-        <button class="banner__button" style="background-color: #FF7C00; color: black;
-         font-weight: 600; font-size: 20px; margin-top: 50px; padding: 5px 10px; border: transparent; border-radius: 5px">${a.text}</button>
+        <button class="button button__orange">${a.text}</button>
     </div>
     `
 }).join(" ");
@@ -37,8 +35,7 @@ let bannerHtmlMidRight = generalBannerRight.map((a) => {
         <div class="banner__img">
             <img class="image" src="${a.img}" alt="photo" />
         </div>
-        <button class="banner__button" style="background-color: #FF7C00; color: black;
-         font-weight: 600; font-size: 20px; margin-top: 50px; padding: 5px 10px; border: transparent; border-radius: 5px">${a.text}</button>
+        <button class="button button__orange">${a.text}</button>
     </div>
     `
 }).join(" ");
