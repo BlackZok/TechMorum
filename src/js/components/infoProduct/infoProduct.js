0c6974b434abf366500f6aa92cd7dc241e176fd4
GogaKler <gogakler@gmail.com>
2022-06-23 00:48:20 +0300

    adaptive filters-info_1

--- src/js/components/infoProduct/infoProduct.js
+++ src/js/components/infoProduct/infoProduct.js
@@ -7,10 +7,10 @@ let infoProductHtml = generalInfoProduct.map((a) => {
     <div class="col-md-4">
         <div class = "infoprod">
 
-            <p style="font-size: 50px;font-weight: 1000;color: white;"> ${a.title} </p>
-            ${a.content.map((cont) => `<p style="color:white; font-size: 16px; font-weight: 500; width: 400px; text-decoration: none;">${cont.text}</p>`).join(" ")}
-            <button style="padding: 5px 10px; font-weight: 500; margin-bottom: 10px; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
-            <button style=" color: white; font-weight: 400; padding: 400px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
+            <p class="text text__big"> ${a.title} </p>
+            ${a.content.map((cont) => `<p style="color:white; font-size: 16px; font-weight: 500;">${cont.text}</p>`).join(" ")}
+            <button class="button button__orange" style="padding: 5px 10px; font-weight: 500; margin-bottom: 10px; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
+            <button class="button button__orange" style=" color: white; font-weight: 400; padding: 400px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
         </div>
     </div>
     `
