0de86ebd7a67126dd5fb670812685e693638bfcf
GogaKler <gogakler@gmail.com>
2022-06-23 01:07:24 +0300

    adaptive expendable-material

--- src/js/components/infoCont/infoCont.js
+++ src/js/components/infoCont/infoCont.js
@@ -5,11 +5,10 @@ const infoCont = document.querySelector('#infoCont');
 let infoContHtml = generalInfoCont.map((a) => {
     return `
     <div class="col-md-4">
-        <div class = "infocot">
-
-            <p style="font-size: 50px; font-weight: 1000; color: white; line-height: 0.9em; padding-bottom: 50px;"> ${a.title} </p>
-            ${a.content.map((cont) => `<p style="color:white; font-size: 16px; font-weight: 500; width: 400px; text-decoration: none;">${cont.text}</p>`).join(" ")}
-            <button style="padding: 5px 10px; font-weight: 500; margin-bottom: 10px; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
+        <div class="infocot">
+            <p class="text text__big" style="line-height: normal"> ${a.title} </p>
+            ${a.content.map((cont) => `<p style="color:white; font-size: 16px; font-weight: 500; text-decoration: none; margin-top: 20px">${cont.text}</p>`).join(" ")}
+            <button style="padding: 5px 10px; font-weight: 500; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
             <button style=" color: white; font-weight: 400; padding: 100px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
         </div>
     </div>
