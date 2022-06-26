5f71e997e1ff2419ad513476a21b2c0eb7131b3b
GogaKler <gogakler@gmail.com>
2022-06-26 22:33:35 +0300

    adaptive footer & add image on the header

--- src/js/components/official/official.js
+++ src/js/components/official/official.js
@@ -6,14 +6,9 @@ let officialHtml = generalOfficial.map((a) => {
     return `
     
         <div class="footer-img" >
-        
-            <img src="${a.img}" alt="" style =" height: 50px; padding: 0 0 10px; display: flex; justify-content: center; text-align: center;">
-
-            <p style=" display: flex; justify-content: center; text-align: left;
-            color: rgba(255,255,255, 0.7); font-size: 16px; font-weight: 300;"> ${a.text} </p>
-
+            <img src="${a.img}" alt="logo">
+            <p style="color: rgba(255,255,255, 0.7); font-weight: 300;"> ${a.text} </p>
         </div>
-    
     `
 }).join(" ");
 
--- src/js/components/underline/underline.js
+++ src/js/components/underline/underline.js
@@ -4,9 +4,8 @@ const underline = document.querySelector('#underline');
 
 let underlineHtml = generalUnderline.map((a) => {
     return `
-    <div class = "footer-underline__info" style="height: 10px;"> 
-        <p style=" display: flex; justify-content: center; text-align: center;
-        color: rgba(255,255,255, 0.7);" >${a.text}</p>
+    <div class = "footer-underline__info"> 
+        <p style="color: rgba(255,255,255, 0.7);" >${a.text}</p>
     </div>
     
     `
