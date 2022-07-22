2e5239dba40cf2ef5064ea9d55a26bbfe5b74903
BlackZok <evgeniy.zok@mail.ru>
2022-07-22 03:36:48 +0300

    final_general_fixed

--- src/js/components/infoBox/infoBox.js
+++ src/js/components/infoBox/infoBox.js
@@ -7,8 +7,8 @@ let infoBoxHtml = generalInfoBox.map((a) => {
     <div class="col-md-4">
         <p class="text text__big" style="line-height: normal">${a.title}</p>
         ${a.content.map((cont) => `<p style="color:white; font-size: 16px; font-weight: 500; margin-top: 20px; text-decoration: none;">${cont.text}</p>`).join(" ")}
-        <button style="padding: 5px 10px; font-weight: 500; margin-bottom: 10px; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
-        <button style=" color: white; font-weight: 400; padding: 200px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
+        <button data-custom-open="order" style="padding: 5px 10px; font-weight: 500; margin-bottom: 10px; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
+        <button data-custom-open="contact" style=" color: white; font-weight: 400; padding: 200px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
     </div>
     `
 }).join(" ");
--- src/js/components/infoCont/infoCont.js
+++ src/js/components/infoCont/infoCont.js
@@ -8,8 +8,8 @@ let infoContHtml = generalInfoCont.map((a) => {
         <div class="infocot">
             <p class="text text__big" style="line-height: normal"> ${a.title} </p>
             ${a.content.map((cont) => `<p style="color:white; font-size: 16px; font-weight: 500; text-decoration: none; margin-top: 20px">${cont.text}</p>`).join(" ")}
-            <button style="padding: 5px 10px; font-weight: 500; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
-            <button style=" color: white; font-weight: 400; padding: 100px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
+            <button data-custom-open="order"  style="padding: 5px 10px; font-weight: 500; background-color: #FF7C00; border: transparent; border-radius: 5px; color: white;">Заказать</button>
+            <button data-custom-open="contact" style=" color: white; font-weight: 400; padding: 100px 10px; background-color: transparent; border: transparent;">Узнать цены</button>
         </div>
     </div>
     `
